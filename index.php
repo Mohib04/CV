986b2fce745b75d3e46626e8ef31bc8e2837927a
Mohibbull Munshi <33421746+Mohib04@users.noreply.github.com>
2022-12-08 18:29:57 +0600

    designing

--- index.php
+++ index.php
@@ -1,211 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Favicon icon-->
-    <link rel="icon" href="images/mohib.png" type="image/png" sizes="16x16">
-    <!-- Bootstrap Css Files -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- Main Css Files -->
-    <link rel="stylesheet" href="css/style.css">
-    <title>Currinculum Vitae Of Mohibbulla Munshi</title>
-</head>
-
-<body>
-
-    <div class="container">
-        <!-- Content here -->
-        <div class="header-section">
-            <h2 class="text-center">CURRICULUM VITAE OF MOHIBBULLA MUNSHI</h2>
-            <hr class="text-center">
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Moddho Basabo,</li>
-                        <li>Sabujbagh,</li>
-                        <li>Dhaka-1214 </li>
-                        <li>Cell: 01757364843</li>
-                        <li>Email: mohib.agu@gmail.com</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <img style="width: 50%; height: 55%;" src="images/mohib.png" class="rounded float-end" alt="mohibbulla's images">
-                </div>
-            </div>
-        </div>
-        <div class="career-objective">
-            <h3>Career Objective</h3>
-            <p>To work for an organization that provides me the opportunity to improve my skills and knowledge to growth along with the organization's objective. An enthusiastic fresher with highly motivated and leadership skills.</p>
-        </div>
-        <div class="academic-qualificaion">
-            <h4>BSc in CSE (Computer Science Engineering)</h4>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Institute :</li>
-                        <li> Department :</li>
-                        <li>Status :</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <ul>
-                        <li>Uttara University.</li>
-                        <li> CSE.</li>
-                        <li>Running (7 semester).</li>
-                    </ul>
-                </div>
-            </div>
-            <h4>Alim/H.S.C(Higher Secondary Certificate)</h4>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Institute :</li>
-                        <li> Group :</li>
-                        <li>Result :</li>
-                        <li> Passing Year :</li>
-                        <li>Board :</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <ul>
-                        <li>Jhalakati N. S. Kamil Madrasah.</li>
-                        <li>Science.</li>
-                        <li>GPA-5.00 out of 5.</li>
-                        <li> 2016</li>
-                        <li>Madrasah.</li>
-                    </ul>
-                </div>
-            </div>
-            <h4>Dakhil/S.S.C (Secondary School Certificate)</h4>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Institute :</li>
-                        <li> Group :</li>
-                        <li>Result :</li>
-                        <li> Passing Year :</li>
-                        <li>Board :</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <ul>
-                        <li>Faridpur Muslim Mission Madrasah.</li>
-                        <li>Science.</li>
-                        <li>GPA-5.00 out of 5.</li>
-                        <li>2014</li>
-                        <li>Madrasah.</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="key-strength">
-            <h3>Area of Key Strength:</h3>
-            <ul>
-                <li>Result Sheet Making and Salary Sheet making.</li>
-                <li>Installing upgrades and new software.</li>
-                <li>Knowledge of Hardware & Software.</li>
-                <li>Working skills with HTML,CSS,WORDPRESS(Web design).</li>
-                <li>Performance tuning for an optimized result.</li>
-                <li>Microsoft Office package(MS Word, Excel, PowerPoint, Access).</li>
-                <li>Graphic design(Photoshop and Illustrator).</li>
-            </ul>
-        </div>
-        <div class="experience">
-            <h3>Job Experience</h3>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Name of Institute :</li>
-                        <li> Designation :</li>
-                        <li>Duration :</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <ul>
-                        <li>দারুচ্ছুন্নাত মোহেব্বিয়া হোসাইনিয়া দ্বীনিয়া মাদরাসা.</li>
-                        <li>Computer Teacher.</li>
-                        <li>5 January 2020 to 10 May 2020.</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="language">
-            <h3>Language Proficiency</h3>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Bengali :</li>
-                        <li> English :</li>
-                        <li>Hindi :</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <ul>
-                        <li>Excellent proficiency in Bengali (Mother Tongue).</li>
-                        <li>Excellent proficiency in English (Reading, Writing, Speaking).</li>
-                        <li>Speaking and listening.</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="personal-details">
-            <h3>Personal Details</h3>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li>Name :</li>
-                        <li>Father’s Name :</li>
-                        <li>Mother’s Name :</li>
-                        <li>Permanent Address :</li>
-                        <li>Date of Birth :</li>
-                        <li>Nationality :</li>
-                        <li>Religion :</li>
-                        <li>Sex :</li>
-                        <li>Marital Status :</li>
-                        <li>Height :</li>
-                        <li>Blood Group :</li>
-                    </ul>
-
-                </div>
-                <div class="col">
-                    <ul>
-                        <li>Mohibbulla Munshi</li>
-                        <li>Abdur Razzak Munshi</li>
-                        <li>Jahanara Begum</li>
-                        <li>Vill: Mukondoputty Post Office: Banorsoddi, Police Station: Nagarkanda, District: Faridpur. Division: Dhaka.
-                        </li>
-                        <li>20 January 1998</li>
-                        <li>Bangladeshi</li>
-                        <li>Islam</li>
-                        <li>Male</li>
-                        <li>Married</li>
-                        <li>5'-6”</li>
-                        <li>O+ (ve)</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <p>I certified that all information in accordance with my curriculum vitae is fully correct.</p>
-        <div class="signature">
-            <h5>Signature</h5>
-            <img src="images/Signature.jpg" alt="">
-            <hr style="width: 27%;color: black;">
-        </div>
+<div class="header-area">
+    <h2>CURRICULUM VITAE OF MOHIBBULLA MUNSHI</h2>
+    <hr>
+    <div class="personal-info">
+        <ul>
+            <li>Address: Arjotpara, Mohakhali, Dhaka-1215</li>
+            <li>Cell: 01757364843</li>
+            <li>Email: mohib.agu@gmail.com</li>
+        </ul>
     </div>
 
-
-
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/index.js"></script>
-</body>
-
-</html>
+</div>
+<div class="career-objective">
+    <h3>Career Objective</h3>
+    <p>To work for an organization that provides me the opportunity to improve my skills and knowledge to
+        growth
+        along with the organization's objective.</p>
+</div>
+<div class="key-strength">
+    <h3>Area of Key Strength:</h3>
+    <ul>
+        <li>Result Sheet Making and Salary Sheet making.</li>
+        <li>Installing upgrades and new software.</li>
+        <li>Knowledge of Hardware & Software.</li>
+        <li>Working skills with HTML,CSS,WORDPRESS(Web design).</li>
+        <li>Performance tuning for an optimized result.</li>
+        <li>Microsoft Office package(MS Word, Excel, PowerPoint, Access).</li>
+        <li>Graphic design(Photoshop and Illustrator).</li>
+    </ul>
+</div>
+<div class="experience">
+    <h3>Job Experience</h3>
+    <ul>
+        <li>Name of Institute : <a href="https://www.anhenterprise.com/">ANH Enterprise Limited</a></li>
+        <li> Designation : Asst. Engineer</li>
+        <li>Duration : 1 Year 0.5 month</li>
+    </ul>
+</div>
+<div class="language">
+    <h3>Language Proficiency</h3>
+    <ul>
+        <li>Bengali : Excellent proficiency in Bengali (Mother Tongue).</li>
+        <li> English : Excellent proficiency in English (Reading, Writing, Speaking).</li>
+        <li>Hindi : Speaking and listening.</li>
+    </ul>
+</div>
+<div class="personal-details">
+    <h3>Personal Details</h3>
+    <ul>
+        <li>Name : Mohibbulla Munshi</li>
+        <li>Father’s Name : Abdur Razzak Munshi</li>
+        <li>Mother’s Name : Jahanara Begum</li>
+        <li>Permanent Address : Vill: Mukondoputty Post Office: Banorsoddi, Police Station: Nagarkanda,
+            District: Faridpur.
+            Division: Dhaka.</li>
+        <li>Date of Birth : 20 January 1998</li>
+        <li>Nationality : Bangladeshi</li>
+        <li>Religion : Islam</li>
+        <li>Sex : Male</li>
+        <li>Marital Status : Married</li>
+        <li>Height : 5'-6”</li>
+        <li>Blood Group : O+ (ve)</li>
+    </ul>
+</div>
+<div class="declartion">
+    <p>I certified that all information in accordance with my curriculum vitae is fully correct.</p>
+</div>
+<div class="signature">
+    <h5>Signature</h5>
+    <img src="images/Signature.jpg" alt="">
+    <hr style="width: 27%;color: black;">
+</div>
